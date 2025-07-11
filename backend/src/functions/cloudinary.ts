15d687d835a39dc0a5470b422d9db5a89401c590
Kunal Singh <kunalindia59@gmail.com>
2025-07-12 00:47:40 +0530

    added a resource_type parameter for video or image upload

--- backend/src/functions/cloudinary.ts
+++ backend/src/functions/cloudinary.ts
@@ -1,9 +1,9 @@
 import cloudinary from "cloudinary"
 const cloud = cloudinary.v2
 cloud.config({
-    cloud_name: 'doyifognr',
-    api_key: '558719477873916',
-    api_secret: 'v8ZvCjyKR-CgQwVd9D8qEpBygxw'
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
 })
 
 interface Result {
@@ -12,10 +12,11 @@ interface Result {
     err?: any
 }
 
-export function uploadAvatar(buffer: Buffer, folder_name: string): Promise<Result> {
+export function uploadAvatar(buffer: Buffer, folder_name: string , resource_type : ("image" | "video")): Promise<Result> {
     return new Promise((res, rej) => {
         const result = cloud.uploader.upload_stream({
-            folder: folder_name
+            folder: folder_name,
+            resource_type : `${resource_type}`
 
         },
             (err, result) => {
