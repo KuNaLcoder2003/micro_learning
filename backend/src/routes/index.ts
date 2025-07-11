4812b7d4e720a5b54f9cee3afa88b388767ac4ae
Kunal Singh <kunalindia59@gmail.com>
2025-07-12 00:48:54 +0530

    added a new course_router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import student_router from "./student"
 import teacher_router from "./teacher"
+import course_router from "./course"
 const router = express.Router()
 
 
 
 router.use("/student" , student_router)
 router.use("/teacher" , teacher_router)
+router.use('/course' , course_router)
 
 
 export default router
